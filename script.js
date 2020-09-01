06980822612bb6797e4cc6b2693706e827f2dbd3
HerrSandybell <talatrach@gmail.com>
2020-08-31 19:54:43 -0600

    reorganized JS. Added keyboard button controls

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Declare DOM variables.
 const documentRoot = document.documentElement;
 const drawingGrid = document.querySelector('.drawing-grid');
 const resetBtn = document.querySelector('#reset');
@@ -8,43 +9,52 @@ const clearBtn = document.querySelector('#clear');
 let control = "mouseover";
 
 const alpha = 0.1;
-let flatColor = `rgba(0,0,0,1)`;
-let shadedColor = `rgba(0,0,0,0.5)`;
+let color = `rgba(0,0,0,1)`;
 
-/**** BUTTON EVENT LISTENERS ****/
+/**** Declare functions ****/
 
 // When reset button is clicked, prompt user for new grid size.
 // If grid size is forced to be empty, set it to 4.
 // Then, generate new Grid.
-resetBtn.addEventListener('click', () => {
+const resetGrid = function() {
   let newSize = prompt('Enter a new grid-');
   if(!newSize) {newSize = 4};
   drawingGrid.innerHTML = "";
   generateGrid(newSize);
-})
+}
 
-// onclick: change etching control type between hover and click.
-controlToggleBtn.addEventListener('click', () => {
+const swapControlType = function() {
   if (control === "mouseover") {
     drawingGrid.removeEventListener(control, etchCell);
     control = "click";
-    controlToggleBtn.textContent = "Hover Etch";
+    controlToggleBtn.textContent = "Hover Etch (e)";
     drawingGrid.addEventListener(control, etchCell);
   } else {
     drawingGrid.removeEventListener(control, etchCell);
     control = "mouseover";
-    controlToggleBtn.textContent = "Click Etch";
+    controlToggleBtn.textContent = `Click Etch (e)`;
     drawingGrid.addEventListener(control, etchCell);
   }
-})
+}
 
-// reset all etched cells.
-clearBtn.addEventListener('click', () => {
+// reset grid cell background to white.
+const clearGridCells = function() {
   let allCells = document.querySelectorAll('.grid-cells');
   allCells.forEach(item => {
     item.style.background = "";
   })
-})
+}
+
+// toggle etched stated on and off
+const etchCell = function(e) {
+  background = e.target.style.background;
+  // e.target.classList.toggle("etched");
+  if(background) {
+    e.target.style.background = "";
+  } else {
+    e.target.style.background = color // should be a function that returns color here;
+  }
+}
 
 /**** GRID CREATION ****/
 
@@ -58,7 +68,6 @@ function createCells(cellCount) {
   }
 
   // attach hover elements to grid parent element and activate the effect on target child. This is more efficient and memory light than adding an event listener to all children.
-  console.log(control)
   drawingGrid.addEventListener(control, etchCell);
 }
 
@@ -68,22 +77,34 @@ function setCellDimensions(cellCount){
   documentRoot.style.setProperty("--cellSize", (1000/cellCount)+"px");
 }
 
-// toggle etched stated on and off
-let etchCell = function(e) {
-  background = e.target.style.background;
-  // e.target.classList.toggle("etched");
-  if(background) {
-    e.target.style.background = "";
-  } else {
-    e.target.style.background = flatColor // should be a function that returns color here;
-  }
-}
-
 // Generate a number of child divs equal to the square of the input.
 function generateGrid(size) {
   createCells(size);
   setCellDimensions(size);
 }
+
+/**** EVENT LISTENERS ****/
+
+// attach click listeners to buttons
+resetBtn.addEventListener('click', resetGrid);
+controlToggleBtn.addEventListener('click', swapControlType);
+clearBtn.addEventListener('click', clearGridCells);
+
+// Add keydown event listeners for all three buttons.
+window.addEventListener('keydown', e => {
+  const key = e.keyCode
+  switch (key) {
+    case 82: // r
+      resetGrid();
+      break;
+    case 69: // e
+      swapControlType();
+      break;
+    case 67: // c
+      clearGridCells();
+      break;
+  }
+})
 
 // Load default 4x4 grid settings.
 window.addEventListener('load', generateGrid(4))
